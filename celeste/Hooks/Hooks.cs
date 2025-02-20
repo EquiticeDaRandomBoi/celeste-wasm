5ac5d790e5d0e41a3dfbd4a1bf1b904258b51bc0
Toshit Chawda <r58playz@gmail.com>
2025-02-19 19:31:48 -0800

    remove forcewindowedhook

--- celeste/Hooks/Hooks.cs
+++ celeste/Hooks/Hooks.cs
@@ -3,13 +3,11 @@ using System.Reflection;
 public class Hooks
 {
     public static BloomHook Bloom;
-    public static ForceWindowedHook Windowed;
     public static CreditsHook Credits;
 
     public static void Initialize(Assembly celeste)
     {
         Bloom = new(celeste);
-        Windowed = new(celeste);
         Credits = new(celeste);
     }
 }
