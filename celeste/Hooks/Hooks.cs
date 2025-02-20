3484b302d2b588654b9f56c9200ad9d9b572e8ef
Toshit Chawda <r58playz@gmail.com>
2025-02-19 19:23:09 -0800

    remove gchook

--- celeste/Hooks/Hooks.cs
+++ celeste/Hooks/Hooks.cs
@@ -2,14 +2,12 @@ using System.Reflection;
 
 public class Hooks
 {
-    public static GCHook Gc;
     public static BloomHook Bloom;
     public static ForceWindowedHook Windowed;
     public static CreditsHook Credits;
 
     public static void Initialize(Assembly celeste)
     {
-        Gc = new();
         Bloom = new(celeste);
         Windowed = new(celeste);
         Credits = new(celeste);
