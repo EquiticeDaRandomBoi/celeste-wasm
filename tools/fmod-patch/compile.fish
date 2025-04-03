c24a382aea960d91ab27e0acc525d0c4b5ea464c
Toshit Chawda <r58playz@gmail.com>
2025-04-02 17:23:36 -0700

    fix docs

--- tools/fmod-patch/compile.fish
+++
@@ -1,9 +0,0 @@
-fish wrap.fish >wrapped.c
-emcc -r wrapped.c -o wrapped.o
-mkdir fmodstudio
-cd fmodstudio
-ar x ../fmodstudio.a
-mv ../wrapped.o .
-emar rc ../fmodstudio-wrapped.a *.o
-cd ..
-rm -r fmodstudio
